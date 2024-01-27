7c6bd70e5176eaf727e30836c104721ffd8ca47c
NBaoBao <64297798+NBaoBao@users.noreply.github.com>
2024-01-27 00:10:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const yesBtnRect = yesBtn.getBoundingClientRect();
+noBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love You Too :)';
+});
+yesBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - yesBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - yesBtnRect.height)) + 1;
+    yesBtn.style.left = i + 'px';
+    yesBtn.style.top = j + 'px';
+});
