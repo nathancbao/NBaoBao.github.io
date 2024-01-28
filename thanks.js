e7b396b3443ee3cb8b3097e8823c2719e5d19425
Kiran J <56204382+KiranJoji@users.noreply.github.com>
2024-01-27 18:17:34 -0800

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -7,16 +7,16 @@ const wrapperRect = wrapper.getBoundingClientRect();
 var sound = new Audio("heheheha.mp3");
 sBtn.addEventListener('click', () => {
     sBtn.style.display = "none";
-    sound = new Audio("heheheha.mp3");
+    sound = new Audio("editstuart.mp3");
     sound.play();
 });
 kBtn.addEventListener('click', () => {
     kBtn.style.display = "none";
-    sound = new Audio("heheheha.mp3");
+    sound = new Audio("editkevin.mp3");
     sound.play();
 });
 bBtn.addEventListener('click', () => {
     bBtn.style.display = "none";
-    sound = new Audio("heheheha.mp3");
+    sound = new Audio("editbob.mp3");
     sound.play();
 });
